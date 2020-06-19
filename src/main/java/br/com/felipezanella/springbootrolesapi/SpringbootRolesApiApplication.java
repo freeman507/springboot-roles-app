25571a0379f3f3f91d2e28406e34b0b0f0a3a6e6
Felipe Zanella <freeman.zanella@gmail.com>
2020-06-19 15:33:33 -0300

    first commit

---
+++ src/main/java/br/com/felipezanella/springbootrolesapi/SpringbootRolesApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipezanella.springbootrolesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRolesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRolesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/felipezanella/springbootrolesapi/SpringbootRolesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipezanella.springbootrolesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRolesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
